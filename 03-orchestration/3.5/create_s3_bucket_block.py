61599f374c06853d327d771e94a67636141c5944
ahairshi <daisyfuentesahamed@gmail.com>
2023-06-15 17:31:32 +0000

    Adds Bucket Details

--- 03-orchestration/3.5/create_s3_bucket_block.py
+++ 03-orchestration/3.5/create_s3_bucket_block.py
@@ -4,7 +4,7 @@ from prefect_aws import S3Bucket, AwsCredentials
 
 def create_aws_creds_block():
     my_aws_creds_obj = AwsCredentials(
-        aws_access_key_id="123abc", aws_secret_access_key="abc123"
+        aws_access_key_id="AKIA57GCNNGU32MJ7WQU", aws_secret_access_key="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"
     )
     my_aws_creds_obj.save(name="my-aws-creds", overwrite=True)
 
@@ -14,7 +14,7 @@ def create_s3_bucket_block():
     my_s3_bucket_obj = S3Bucket(
         bucket_name="my-first-bucket-abc", credentials=aws_creds
     )
-    my_s3_bucket_obj.save(name="s3-bucket-example", overwrite=True)
+    my_s3_bucket_obj.save(name="zoomcamp-mlops-2023-taxi", overwrite=True)
 
 
 if __name__ == "__main__":
